cce002a78ad554207e9f8122d8c3a8b0de5c8689
Kapanshya Swaba <82084573+Kapanshya-Swaba@users.noreply.github.com>
2024-09-27 08:19:30 +0200

    Update server.js
    changed route / to send index file

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = Express();
 const port = 8000;
 
 app.get('/', function(req, res) {
-    res.send('<h1 style = " color:green; text-align:center; text-transform:uppercase; position:relative; top: 50%;  font-size:50px;" > 404 <br> File not found </h1>')
+    res.sendFile(__dirname + '/public/index.html')
 
-})
+});
+
 app.listen(port, function(){
     console.log('server is running at ${port}')
 
